19010a08a5011d36b1d09e78e987e4186c20627b
Yi Liu <eyi.liu@outlook.com>
2022-07-02 08:17:50 +0000

    include linenoise mode

--- comet_wsf.cc
+++ comet_wsf.cc
@@ -54,16 +54,6 @@ void map_mem_block(int* p_fd, uint32_t paddr, uint32_t psize, uint32_t** pp_vbas
 
 
 
-static const std::string help_usage = R"(
-Usage:
-comet_wsf_test <addresss_base>
-
-examples:
-./comet_wsf 0x43c00000
-)";
-
-
-
 #define R_DAQB_PBASE    0x43c00000
 #define D_DAQB_PSIZE    0x100
 
@@ -74,7 +64,7 @@ examples:
 #define R_DAQB_TEMP             0x10
 #define R_DAQB_HVOLT            0x14
 
-#define U_TEMP_C_PER            0.0078125 
+#define U_TEMP_C_PER            0.0078125
 #define U_DAC_V_PER             0.0008059
 
 #define R_FEB_RESET             0x0000
@@ -95,14 +85,6 @@ examples:
 
 
 uint32_t *pR_DAQB_VBASE32 = 0;
-uint32_t *pR_DAQB_CMD_HADDR_LDATA = 0;
-uint32_t *pR_DAQB_DATA_RESET = 0;
-uint32_t *pR_DAQB_DATA_RUN = 0;
-uint32_t *pR_DAQB_CMD_ACK = 0;
-uint32_t *pR_DAQB_TEMP = 0;
-uint32_t *pR_DAQB_HVOLT = 0;
-
-
 
 
 void daqb_reg_write(uint32_t o, uint32_t d, uint32_t* vbase){
@@ -111,25 +93,28 @@ void daqb_reg_write(uint32_t o, uint32_t d, uint32_t* vbase){
 
 
 uint32_t daqb_reg_read(uint32_t o, uint32_t* vbase){
-  return *(vbase+o);
+  uint32_t data =  *(vbase+o);
+  if(o!=R_DAQB_CMD_ACK){
+    std::printf("read DAQB register:  %#010x @ %#010x \n", data, o);
+  }
+  return data;
 }
 
 void daqb_reg_write(uint32_t o, uint32_t d){
   uint32_t *vbase = pR_DAQB_VBASE32;
-  *(vbase+o) = d;
+  daqb_reg_write(o, d, vbase);
 }
-
 
 uint32_t daqb_reg_read(uint32_t o){
   uint32_t *vbase = pR_DAQB_VBASE32;
-  return *(vbase+o);
+  return daqb_reg_read(o, vbase);
 }
 
 
 void feb_cmd_write(uint16_t a, uint16_t d){
   uint32_t cmd = a<<16 + d;
   std::printf("writing DAQB register:  %#010x @ %#010x \n", d, a);
-  daqb_reg_write(R_DAQB_CMD_HADDR_LDATA, cmd);  
+  daqb_reg_write(R_DAQB_CMD_HADDR_LDATA, cmd);
 }
 
 
@@ -142,7 +127,7 @@ void daqb_wait_for_ack_cnt(uint32_t last_cnt){
   auto tp_timeout = tp_start + 2s;
   for(uint32_t cnt_new = daqb_reg_read(R_DAQB_CMD_ACK); cnt_new ==last_cnt; ){
     if(std::chrono::steady_clock::now()>tp_timeout){
-      std::printf("timetout: %#010x,  %#010x \n", last_cnt, *pR_DAQB_CMD_ACK);
+      std::printf("timetout: last ack cnt %#010x,  %#010x \n", last_cnt, cnt_new);
       throw;
     }
     std::this_thread::sleep_for(20ms);
@@ -157,12 +142,12 @@ void feb_read_new_temp(){
   daqb_wait_for_ack_cnt(last_cmd_ack_cnt);
   uint32_t temp_new = daqb_reg_read(R_DAQB_TEMP);
   std::printf("temp raw new %#010x, last %#010x \n", temp_new, last_temp);
-  std::printf("temp     new %u, last %u \n", temp_new*U_TEMP_C_PER, last_temp*U_TEMP_C_PER);  
+  std::printf("temp     new %u, last %u \n", temp_new*U_TEMP_C_PER, last_temp*U_TEMP_C_PER);
 }
 
 
 void feb_set_hv_voltage(){
-  // TODO  
+  // TODO
 }
 
 void feb_set_hv_raw(){
@@ -181,33 +166,57 @@ void feb_set_dac_voltage(uint32_t ch, double v){
 }
 
 
+static  const std::string help_usage
+(R"(
+Usage:
+-h : print usage information, and then quit
 
-int main(int argc, char** argv){
-  off_t addrbase_phy = R_DAQB_PBASE; 
-  if(argc == 1){
-    std::fprintf(stdout, "%s\n", help_usage.c_str());
+'help' command in interactive mode provides detail usage information
+)"
+ );
 
-    std::fprintf(stdout, "using the default base: %#010x\n", (uint32_t)addrbase_phy);
-    
+
+static  const std::string help_usage_linenoise
+(R"(
+
+keyword: help, print, init, reset, quit, daqb, feb, set, get, cmd, hvolt, dac, temp, asic0, asic1, raw, volt
+example:
+  1) quit command line
+   > quit
+
+  2) get daqb regiester
+   > daqb get raw [nAddress]
+
+  3) set daqb regiester
+   > daqb set raw [nAddress] [nData]
+
+  4) set FEB register
+   > feb set raw [nAddress] [nData]
+
+  5) get temp module Celsius
+   > temp get
+
+)"
+ );
+
+
+int main(int argc, char **argv){
+  std::string c_opt;
+  int c;
+  while ( (c = getopt(argc, argv, "h")) != -1) {
+    switch (c) {
+    case 'h':
+      fprintf(stdout, "%s", help_usage.c_str());
+      return 0;
+      break;
+    default:
+      fprintf(stderr, "%s", help_usage.c_str());
+      return 1;
+    }
   }
-  else{
-    if(argc==2){
-    std::fprintf(stdout, "%s\n", help_usage.c_str());
-    std::exit(-1);
-    }
 
-    if(std::regex_match(argv[1], std::regex("\\s*(?:(0[Xx])?([0-9a-fA-F]+))\\s*")) ){
-      std::cmatch mt;
-      std::regex_match(argv[1], mt, std::regex("\\s*(?:(0[Xx])?([0-9a-fA-F]+))\\s*"));
-      addrbase_phy = std::stoull(mt[2].str(), 0, mt[1].str().empty()?10:16);
-    }
-    else{
-      std::fprintf(stderr, "%s is not a valid address\n", argv[1]);
-      std::fprintf(stdout, "%s\n", help_usage.c_str());
-      std::exit(-1);
-    }
-  }
 
+  off_t addrbase_phy = R_DAQB_PBASE;
   int fd;
   pR_DAQB_VBASE32 = 0;
   map_mem_block(&fd, addrbase_phy, D_DAQB_PSIZE, &pR_DAQB_VBASE32);
@@ -217,7 +226,79 @@ int main(int argc, char** argv){
     exit(-1);
   }
 
-  
+
+  const char* linenoise_history_path = "/tmp/.linenoiseng_comet_wsf";
+  linenoiseHistoryLoad(linenoise_history_path);
+  linenoiseSetCompletionCallback([](const char* prefix, linenoiseCompletions* lc)
+                                 {
+                                   static const char* examples[] =
+                                     {"help", "print", "init", "reset", "quit", "daqb", "feb", "set", "get", "cmd", "hvolt", "dac", "temp", "asic0", "asic1", "raw", "volt",
+                                      NULL};
+                                   size_t i;
+                                   for (i = 0;  examples[i] != NULL; ++i) {
+                                     if (strncmp(prefix, examples[i], strlen(prefix)) == 0) {
+                                       linenoiseAddCompletion(lc, examples[i]);
+                                     }
+                                   }
+                                 } );
+
+  const char* prompt = "\x1b[1;32mcomet_wsf\x1b[0m> ";
+  while (1) {
+    char* result = linenoise(prompt);
+    if (result == NULL) {
+      break;
+    }
+    if ( std::regex_match(result, std::regex("\\s*(help)\\s*")) ){
+      fprintf(stdout, "%s", help_usage_linenoise.c_str());
+    }
+    else if ( std::regex_match(result, std::regex("\\s*(quit)\\s*")) ){
+      printf("quiting \n");
+      linenoiseHistoryAdd(result);
+      free(result);
+      break;
+    }
+    else if ( std::regex_match(result, std::regex("\\s*(reset)\\s*")) ){
+      printf("reset TODO\n");
+      //TODO
+    }
+    else if ( std::regex_match(result, std::regex("\\s*(init)\\s*")) ){
+      printf("init TODO\n");
+      //TODO
+    }
+
+    else if ( std::regex_match(result, std::regex("\\s*(daqb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s+(?:(0[Xx])?([0-9]+))\\s*")) ){
+      std::cmatch mt;
+      std::regex_match(result, mt, std::regex("\\s*(daqb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s+(?:(0[Xx])?([0-9]+))\\s*"));
+      uint64_t addr = std::stoull(mt[5].str(), 0, mt[4].str().empty()?10:16);
+      uint64_t data = std::stoull(mt[7].str(), 0, mt[6].str().empty()?10:16);
+      daqb_reg_write(addr, data);
+    }
+    else if ( std::regex_match(result, std::regex("\\s*(daqb)\\s+(get)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s*")) ){
+      std::cmatch mt;
+      std::regex_match(result, mt, std::regex("\\s*(daqb)\\s+(get)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s*"));
+      uint64_t addr = std::stoull(mt[5].str(), 0, mt[4].str().empty()?10:16);
+      daqb_reg_read(addr);
+    }
+
+    else if ( std::regex_match(result, std::regex("\\s*(feb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s+(?:(0[Xx])?([0-9]+))\\s*")) ){
+      std::cmatch mt;
+      std::regex_match(result, mt, std::regex("\\s*(feb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s+(?:(0[Xx])?([0-9]+))\\s*"));
+      uint64_t addr = std::stoull(mt[5].str(), 0, mt[4].str().empty()?10:16);
+      uint64_t data = std::stoull(mt[7].str(), 0, mt[6].str().empty()?10:16);
+      feb_cmd_write(addr, data);
+    }
+    else if ( std::regex_match(result, std::regex("\\s*(temp)\\s+(get)\\s*")) ){
+      feb_read_new_temp();
+    }
+
+
+    linenoiseHistoryAdd(result);
+    free(result);
+  }
+
+  linenoiseHistorySave(linenoise_history_path);
+  linenoiseHistoryFree();
+
   close(fd);
   return 0;
 }
