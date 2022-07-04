196eb3da08b38a2256391746493693e8a81ca5be
Yi Liu <eyi.liu@outlook.com>
2022-07-04 08:17:02 +0000

    fix hex option

--- comet_wsf.cc
+++ comet_wsf.cc
@@ -22,13 +22,42 @@
 
 #include "linenoise.h"
 
+#ifndef DEBUG_OFFLINE
+#define DEBUG_OFFLINE 0
+#endif
 
+#ifndef DEBUG_PRINT
+#define DEBUG_PRINT 1
+#endif
+#define debug_print(fmt, ...)                                           \
+  do { if (DEBUG_PRINT) std::fprintf(stdout, fmt, ##__VA_ARGS__); } while (0)
+
+#ifndef INFO_PRINT
+#define INFO_PRINT 0
+#endif
+#define info_print(fmt, ...)                                           \
+  do { if (INFO_PRINT) std::fprintf(stdout, fmt, ##__VA_ARGS__); } while (0)
+
+
+
+using std::int8_t;
+using std::int16_t;
+using std::int32_t;
+using std::uint8_t;
+using std::uint16_t;
 using std::uint32_t;
 using std::size_t;
 using namespace std::chrono_literals;
 
 
 void map_mem_block(int* p_fd, uint32_t paddr, uint32_t psize, uint32_t** pp_vbase32){
+  if(DEBUG_OFFLINE){
+    *p_fd = 0;
+    *pp_vbase32 = new uint32_t[(psize+3)/4];
+    debug_print("emulate a memory block for offline debug.\n");
+    return;
+  }
+
   if ((*p_fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0 ) {
     std::fprintf(stderr, "Error opening file. \n");
     throw;
@@ -52,6 +81,40 @@ void map_mem_block(int* p_fd, uint32_t paddr, uint32_t psize, uint32_t** pp_vbas
 }
 
 
+void ummap_mem_block(int* p_fd, uint32_t** pp_vbase32){
+  if(DEBUG_OFFLINE){
+    *p_fd = 0;
+    delete [] (*pp_vbase32);
+    *pp_vbase32 = 0;
+    debug_print("close emulated a memory block under offline debug.\n");
+    return;
+  }
+  close(*p_fd);
+  *p_fd = 0;
+  *pp_vbase32 = 0;
+  return;
+}
+
+
+namespace{
+  std::string CStringToHexString(const char *bin, int len){
+    constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                               '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    const unsigned char* data = (const unsigned char*)(bin);
+    std::string s(len * 2, ' ');
+    for (int i = 0; i < len; ++i) {
+      s[2 * i]     = hexmap[(data[i] & 0xF0) >> 4];
+      s[2 * i + 1] = hexmap[data[i] & 0x0F];
+    }
+    return s;
+  }
+
+  std::string StringToHexString(const std::string bin){
+    return CStringToHexString(bin.data(), bin.size());
+  }
+}
+
+
 
 
 #define R_DAQB_PBASE    0x43c00000
@@ -66,6 +129,8 @@ void map_mem_block(int* p_fd, uint32_t paddr, uint32_t psize, uint32_t** pp_vbas
 
 #define U_TEMP_C_PER            0.0078125
 #define U_DAC_V_PER             0.0008059
+#define U_HVOLT_V_PER           0.001812
+
 
 #define R_FEB_RESET             0x0000
 #define R_FEB_DAC_CONF          0x0001
@@ -94,7 +159,10 @@ void daqb_reg_write(uint32_t o, uint32_t d, uint32_t* vbase){
 
 uint32_t daqb_reg_read(uint32_t o, uint32_t* vbase){
   uint32_t data =  *(vbase+o);
-  if(o!=R_DAQB_CMD_ACK){
+  if(o==R_DAQB_CMD_ACK){
+    std::printf("monitoring DAQB ACK:  %#010x @ %#010x \n", data, o);
+  }
+  else{
     std::printf("read DAQB register:  %#010x @ %#010x \n", data, o);
   }
   return data;
@@ -123,6 +191,11 @@ uint32_t daqb_read_last_ack_cnt(){
 }
 
 void daqb_wait_for_ack_cnt(uint32_t last_cnt){
+  if(DEBUG_OFFLINE){
+    debug_print("offline emulation: increase R_DAQB_CMD_ACK, set it to %u\n", last_cnt+1);
+    daqb_reg_write(R_DAQB_CMD_ACK, last_cnt+1);
+  }
+
   auto tp_start = std::chrono::steady_clock::now();
   auto tp_timeout = tp_start + 2s;
   for(uint32_t cnt_new = daqb_reg_read(R_DAQB_CMD_ACK); cnt_new ==last_cnt; ){
@@ -135,19 +208,36 @@ void daqb_wait_for_ack_cnt(uint32_t last_cnt){
   }
 }
 
-void feb_read_new_temp(){
-  uint32_t last_temp = daqb_reg_read(R_DAQB_TEMP);
+double feb_read_new_temp(){
+  uint32_t last_temp_ascii = daqb_reg_read(R_DAQB_TEMP);
   uint32_t last_cmd_ack_cnt = daqb_read_last_ack_cnt();
   feb_cmd_write(R_FEB_TEMP_FETCH, 1);
   daqb_wait_for_ack_cnt(last_cmd_ack_cnt);
-  uint32_t temp_new = daqb_reg_read(R_DAQB_TEMP);
-  std::printf("temp raw new %#010x, last %#010x \n", temp_new, last_temp);
-  std::printf("temp     new %u, last %u \n", temp_new*U_TEMP_C_PER, last_temp*U_TEMP_C_PER);
+  uint32_t temp_ascii = daqb_reg_read(R_DAQB_TEMP);
+  std::printf("temp raw new %#010x, last %#010x \n", temp_ascii, last_temp_ascii);
+
+  std::string temp_str(reinterpret_cast<const char*>(&temp_ascii), 4);
+  uint32_t temp_div =  std::stoul(temp_str, 0, 16);
+  double temp = temp_div * U_TEMP_C_PER;
+
+  std::printf("temp new   %u * U_TEMP_C_PER = %f Celsius \n", temp_div, temp);
+
+  return temp;
 }
 
 
-void feb_set_hv_voltage(){
-  // TODO
+void feb_set_hv_voltage(double v){
+  // TODO, assuming positive only
+  uint16_t hvseti = lrint(v/U_HVOLT_V_PER);
+  char hvset_h = (hvseti>>8 & 0x00ff);
+  char hvset_l = (hvseti & 0x00ff);
+  std::string hvset_hs = CStringToHexString(&hvset_h, 1);
+  std::string hvset_ls = CStringToHexString(&hvset_l, 1);
+  uint16_t hvset_hi = (uint16_t(uint8_t(hvset_hs[0])))<<8 + (uint16_t(uint8_t(hvset_hs[1])));
+  uint16_t hvset_li = (uint16_t(uint8_t(hvset_ls[0])))<<8 + (uint16_t(uint8_t(hvset_ls[1])));
+
+  double temp = feb_read_new_temp();
+
 }
 
 void feb_set_hv_raw(){
@@ -162,8 +252,46 @@ void feb_read_hv_raw(){
 void feb_set_dac_voltage(uint32_t ch, double v){
   uint32_t v_raw = lrint(v/U_DAC_V_PER);
   uint32_t data_raw = (ch & 0xff) << 12 + (v_raw & 0xffffff);
+
+  uint32_t last_ack_cnt;
+  last_ack_cnt = daqb_read_last_ack_cnt();
   feb_cmd_write(R_FEB_DAC_CONF, data_raw);
+  daqb_wait_for_ack_cnt(last_ack_cnt);
+
+  last_ack_cnt = daqb_read_last_ack_cnt();
+  feb_cmd_write(R_FEB_DAC_CONF, 1);
+  daqb_wait_for_ack_cnt(last_ack_cnt);
 }
+
+
+void feb_set_asic_raw(uint32_t ch, uint16_t data){
+  if(ch>1){
+    std::printf("Error: asic%u does not exist.\n", ch);
+    return;
+  }
+
+  uint32_t last_ack_cnt;
+  last_ack_cnt = daqb_read_last_ack_cnt();
+  if(ch==0){
+    feb_cmd_write(R_FEB_ASIC0_CONF, data);
+  }
+  else{
+    feb_cmd_write(R_FEB_ASIC1_CONF, data);
+  }
+  daqb_wait_for_ack_cnt(last_ack_cnt);
+
+  last_ack_cnt = daqb_read_last_ack_cnt();
+  if(ch==0){
+    feb_cmd_write(R_FEB_ASIC0_CONF_PUSH, 1);
+  }
+  else{
+    feb_cmd_write(R_FEB_ASIC1_CONF_PUSH, 1);
+  }
+  daqb_wait_for_ack_cnt(last_ack_cnt);
+
+}
+
+
 
 
 static  const std::string help_usage
@@ -179,7 +307,7 @@ Usage:
 static  const std::string help_usage_linenoise
 (R"(
 
-keyword: help, print, init, reset, quit, daqb, feb, set, get, cmd, hvolt, dac, temp, asic0, asic1, raw, volt
+keyword: help, print, init, reset, quit, daqb, feb, set, get, cmd, hvolt, dac, temp, asic, raw, volt
 example:
   1) quit command line
    > quit
@@ -195,6 +323,12 @@ example:
 
   5) get temp module Celsius
    > temp get
+
+  6) set&push FEB DAC voltage
+   > dac set [nChannel] [nVolt]
+
+  7) set&push FEB ASIC raw configure
+   > asic set raw [nChannel] [nData]
 
 )"
  );
@@ -217,12 +351,12 @@ int main(int argc, char **argv){
 
 
   off_t addrbase_phy = R_DAQB_PBASE;
-  int fd;
+  int fd = 0;
   pR_DAQB_VBASE32 = 0;
   map_mem_block(&fd, addrbase_phy, D_DAQB_PSIZE, &pR_DAQB_VBASE32);
-  if(fd==0 || pR_DAQB_VBASE32==0){
+  if(pR_DAQB_VBASE32==0){
     std::fprintf(stderr, "Memory mapped failed\n");
-    close(fd);
+    ummap_mem_block(&fd, &pR_DAQB_VBASE32);
     exit(-1);
   }
 
@@ -232,7 +366,7 @@ int main(int argc, char **argv){
   linenoiseSetCompletionCallback([](const char* prefix, linenoiseCompletions* lc)
                                  {
                                    static const char* examples[] =
-                                     {"help", "print", "init", "reset", "quit", "daqb", "feb", "set", "get", "cmd", "hvolt", "dac", "temp", "asic0", "asic1", "raw", "volt",
+                                     {"help", "print", "init", "reset", "quit", "daqb", "feb", "set", "get", "cmd", "hvolt", "dac", "temp", "asic", "raw", "volt",
                                       NULL};
                                    size_t i;
                                    for (i = 0;  examples[i] != NULL; ++i) {
@@ -266,23 +400,23 @@ int main(int argc, char **argv){
       //TODO
     }
 
-    else if ( std::regex_match(result, std::regex("\\s*(daqb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s+(?:(0[Xx])?([0-9]+))\\s*")) ){
+    else if ( std::regex_match(result, std::regex("\\s*(daqb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s*")) ){
       std::cmatch mt;
-      std::regex_match(result, mt, std::regex("\\s*(daqb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s+(?:(0[Xx])?([0-9]+))\\s*"));
+      std::regex_match(result, mt, std::regex("\\s*(daqb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s*"));
       uint64_t addr = std::stoull(mt[5].str(), 0, mt[4].str().empty()?10:16);
       uint64_t data = std::stoull(mt[7].str(), 0, mt[6].str().empty()?10:16);
       daqb_reg_write(addr, data);
     }
-    else if ( std::regex_match(result, std::regex("\\s*(daqb)\\s+(get)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s*")) ){
+    else if ( std::regex_match(result, std::regex("\\s*(daqb)\\s+(get)\\s+(raw)\\s+(?:(0[Xx])?([0-9a-fA-F]+))    )\\s*")) ){
       std::cmatch mt;
-      std::regex_match(result, mt, std::regex("\\s*(daqb)\\s+(get)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s*"));
+      std::regex_match(result, mt, std::regex("\\s*(daqb)\\s+(get)\\s+(raw)\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s*"));
       uint64_t addr = std::stoull(mt[5].str(), 0, mt[4].str().empty()?10:16);
       daqb_reg_read(addr);
     }
 
-    else if ( std::regex_match(result, std::regex("\\s*(feb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s+(?:(0[Xx])?([0-9]+))\\s*")) ){
+    else if ( std::regex_match(result, std::regex("\\s*(feb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s*")) ){
       std::cmatch mt;
-      std::regex_match(result, mt, std::regex("\\s*(feb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9]+))\\s+(?:(0[Xx])?([0-9]+))\\s*"));
+      std::regex_match(result, mt, std::regex("\\s*(feb)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s*"));
       uint64_t addr = std::stoull(mt[5].str(), 0, mt[4].str().empty()?10:16);
       uint64_t data = std::stoull(mt[7].str(), 0, mt[6].str().empty()?10:16);
       feb_cmd_write(addr, data);
@@ -291,6 +425,25 @@ int main(int argc, char **argv){
       feb_read_new_temp();
     }
 
+    else if ( std::regex_match(result, std::regex("\\s*(dac)\\s+(set)\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s*")) ){
+      std::cmatch mt;
+      std::regex_match(result, mt, std::regex("\\s*(dac)\\s+(set)\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s*"));
+      uint64_t ch = std::stoull(mt[4].str(), 0, mt[3].str().empty()?10:16);
+      uint64_t voltage = std::stoull(mt[6].str(), 0, mt[5].str().empty()?10:16);
+      feb_set_dac_voltage(ch, voltage);
+    }
+
+    else if ( std::regex_match(result, std::regex("\\s*(asic)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s*")) ){
+      std::cmatch mt;
+      std::regex_match(result, mt, std::regex("\\s*(asic)\\s+(set)\\s+(raw)\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s+(?:(0[Xx])?([0-9a-fA-F]+))\\s*"));
+      uint64_t ch = std::stoull(mt[5].str(), 0, mt[4].str().empty()?10:16);
+      uint64_t data = std::stoull(mt[7].str(), 0, mt[6].str().empty()?10:16);
+      feb_set_asic_raw(ch, data);
+    }
+
+    else{
+      std::printf("Error, unknown command:    %s\n", result);
+    }
 
     linenoiseHistoryAdd(result);
     free(result);
@@ -299,6 +452,6 @@ int main(int argc, char **argv){
   linenoiseHistorySave(linenoise_history_path);
   linenoiseHistoryFree();
 
-  close(fd);
+  ummap_mem_block(&fd, &pR_DAQB_VBASE32);
   return 0;
 }
